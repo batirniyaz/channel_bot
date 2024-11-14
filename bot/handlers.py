effa0e7168a881aec7d4aedc9bc2a0632eb96d2f
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-11-14 14:42:48 +0500

    add new handler to get chat or group id

--- bot/handlers.py
+++ bot/handlers.py
@@ -6,3 +6,11 @@ from telebot import types
 def bot_start(message: types.Message):
     bot.reply_to(message, 'Hi, I was created by BatyaMoget. I am saying to him million thanks')
 
+
+@bot.message_handler(commands=['get_id'])
+def get_id(message: types.Message):
+    cid = message.chat.id
+    mid = message.message_id
+    fid = message.from_user.id
+    bot.send_message(message.chat.id, '\n'.join([f'cid: {cid}', f'mid: {mid}', f'fid: {fid}']))
+
