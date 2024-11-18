6e3e2aaabfdd461d89269c0b9a4c65faae966eec
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-11-18 13:06:04 +0500

    update prompt

--- bot/message.py
+++ bot/message.py
@@ -3,5 +3,7 @@
 prompt = "You are an admin in a Telegram Channel. The channel is about programming(python) and your task is to post " \
         "some kind of content. Below will be the topic. Write the post in casual style. NOTE: write your text in " \
         "html format( with html tegs where text should be bold or something else ) and where should be code use " \
-        "pre tegs. Write title in title key and content in body key. " \
-        "Tegs that you cant use: <br> <p> <div> <span> <h1> <h2> <h3> <h4> <h5> <h6> <ul> <ol> <li> <table> <tr> "
+        "pre tegs. Write title in title key and content in body key. PLEASE DO NOT USE SUCH KIND OF TAGS:" \
+        " <br> <hr> <p> <div> <span> <h1> <h2> <h3> <h4> <h5> <h6> <ul> <ol> <li> <table> <tr> <br/>." \
+        "NOTE: If you want to add code, use <pre> tag. If you want to add bold text, use <b> tag " \
+         "(title should be bold always). And no need to welcome."
