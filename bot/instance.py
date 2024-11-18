c3c155d5d810295148cebdd12253d6677f7e87f7
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-11-18 11:12:05 +0500

    update admin filter

--- bot/instance.py
+++ bot/instance.py
@@ -1,7 +1,16 @@
 import telebot
 from config import TOKEN
-from .filter import IsAdmin
+from config import CHANNEL_ID
 
 bot = telebot.TeleBot(TOKEN, threaded=False)
+
+
+class IsAdmin(telebot.custom_filters.SimpleCustomFilter):
+    key = 'is_admin'
+
+    @staticmethod
+    def check(message: telebot.types.Message):
+        return bot.get_chat_member('-100' + CHANNEL_ID, message.from_user.id).status in ['administrator', 'creator']
+
 
 bot.add_custom_filter(IsAdmin())
