87bf978dccd52f90273479063e2c243edc426bb9
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-11-18 11:12:15 +0500

    validate user for admin

--- bot/handlers.py
+++ bot/handlers.py
@@ -13,7 +13,7 @@ def bot_start(message: types.Message):
     bot.reply_to(message, 'Hi, I was created by BatyaMoget. I am saying to him million thanks')
 
 
-@bot.message_handler(commands=['get_id'])
+@bot.message_handler(is_admin=True, commands=['get_id'])
 def get_id(message: types.Message):
     cid = message.chat.id
     mid = message.message_id
@@ -21,15 +21,16 @@ def get_id(message: types.Message):
     bot.send_message(message.chat.id, '\n'.join([f'cid: {cid}', f'mid: {mid}', f'fid: {fid}']))
 
 
-@bot.message_handler(commands=['post'])
+@bot.message_handler(is_admin=True, commands=['post'])
 def handle_post(message: types.Message):
     bot.send_message(message.chat.id, 'Please write the topic of the post')
     bot.register_next_step_handler(message, foo)
 
 
-@bot.message_handler(chat_types=['private'])
+@bot.message_handler(is_admin=True, chat_types=['private'])
 def foo(message: types.Message):
-    bot.send_message(message.chat.id, 'Seconds please... \t 🕒Generating post... 🕒')
+    bot.send_message(message.chat.id, 'Seconds please... 🕒\tGenerating post... 🕒')
     generated_text = generate(message.text)
     bot.send_message(id_channel, generated_text, parse_mode='HTML')
+    bot.send_message(message.chat.id, 'Post has been sent to the target channel')
 
